74d69a482427938cc0d1efd52daa030914a11e55
BBBOND <jwy8645@163.com>
2016-03-10 10:54:46 +0800

    用户服务层

--- src/main/java/com/geekmadmen/secret/service/UserServiceI.java
+++ src/main/java/com/geekmadmen/secret/service/UserServiceI.java
@@ -1,4 +1,13 @@
 package com.geekmadmen.secret.service;
 
-public class UserServiceI {
+import com.geekmadmen.secret.UImodel.User;
+
+import java.io.Serializable;
+
+public interface UserServiceI {
+    Serializable addUser(User user);
+
+    User login(User user);
+
+
 }
---
+++ src/main/java/com/geekmadmen/secret/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.geekmadmen.secret.service;
+
+import org.springframework.stereotype.Service;
+
+@Service(value = "userService")
+public class UserServiceImpl implements UserServiceI {
+
+}
