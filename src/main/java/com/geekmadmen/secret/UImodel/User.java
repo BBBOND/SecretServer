50b741709daedaaa91ed6529be467d7ec2421714
BBBOND <jwy8645@163.com>
2016-03-10 10:21:24 +0800

    新增UI实体

---
+++ src/main/java/com/geekmadmen/secret/UImodel/User.java
@@ -0,0 +1,89 @@
+package com.geekmadmen.secret.UImodel;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class User implements Serializable {
+
+    private String userId;
+    private String userName;
+    private String userPassword;
+    private String userNickname;
+    private String userAvatar;
+    private String phone;
+    private String email;
+    private String register;
+    private Timestamp createTime;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegister() {
+        return register;
+    }
+
+    public void setRegister(String register) {
+        this.register = register;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+}
